966c2bff50d8b9cda521a2e3f96fdb9f65c97348
choyoona <dbskwkd1235788@gmail.com>
2022-08-17 18:43:35 +0900

    quiz

---
+++ quiz.py
@@ -0,0 +1,130 @@
+from lib2to3.pgen2 import pgen
+import pygame
+import random
+# 기본 초기화 (반드시 해야 하는 것들)
+
+pygame.init() 
+
+# 화면 크기 설정
+screen_width = 480 
+screen_height = 640 
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("똥 피하기") # 게임 이름
+
+
+clock = pygame.time.Clock()
+
+# 1. 사용자 게임 초기화 (배경 화면, 게임 이미지, 좌표, 속도, 폰트 등)
+
+background = pygame.image.load("/Users/yoona/Documents/Python/pygame_bas/background.png")
+
+character = pygame.image.load("/Users/yoona/Documents/Python/pygame_bas/zzio.png")
+character_size = character.get_rect().size # 이미지의 크기를 구해옴
+character_width = character_size[0]  # 캐릭터의 가로 크기
+character_height = character_size[1] # 캐릭터의 세로 크기
+character_x_pos = random.randrange(480) # (screen_width / 2) - (character_width / 2) 
+character_y_pos = screen_height - character_height  # 화면 세로 크기 가장 아래에 해당되는 곳에 위치
+character_speed = 1
+
+ddong = pygame.image.load("/Users/yoona/Documents/Python/pygame_bas/enemy.png")
+ddong_size = ddong.get_rect().size # 이미지의 크기를 구해옴
+ddong_width = ddong_size[0]  # 캐릭터의 가로 크기
+ddong_height = ddong_size[1] # 캐릭터의 세로 크기
+ddong_x_pos = random.randrange(480)  # random.randint(0, screen_width - ddong_width)
+ddong_y_pos = 0 
+ddong_speed = 10
+
+# 이동할 좌표
+to_x = 0
+to_y = 0
+
+
+# 이번트 루프
+running = True # 게임이 진행중인가를 확인
+while running :
+    dt = clock.tick(30) # 게임 화면의 초당 프레임 수를 결정 
+
+    # 2. 이벤트 처리 (키보드, 마우스 등)
+    for event in pygame.event.get():  # 어떤 이벤트가 발생하였는가?
+        if event.type == pygame.QUIT: # 창을 닫는 버튼을 누르면
+            running = False  # 게임이 진행중이 아님
+
+        if event.type == pygame.KEYDOWN:      # 키가 눌러졌는지 확인
+            if event.key == pygame.K_LEFT:    # 캐릭터를 왼쪽으로
+                to_x -= character_speed  # to_x = to_x -5
+            elif event.key == pygame.K_RIGHT: # 캐릭터를 오른쪽으로 
+                to_x += character_speed
+
+
+        if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+
+
+
+    # 3. 게임 캐릭터 위치 정의
+    
+    character_x_pos += to_x * dt  # fps 변화에 상관없이 게임 속도 일정하게 유지시킬 수 있음 
+    character_y_pos += to_y * dt
+
+    # 캐릭터 화면 밖으로 못나가게
+    # 가로 경계값 처리 
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    ddong_y_pos += ddong_speed
+
+    if ddong_y_pos > screen_height:
+        ddong_y_pos = 0
+        ddong_x_pos = random.randrange(480)
+
+    # 충돌 처리를 위한 rect 정보 업데이트 
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = ddong.get_rect()
+    enemy_rect.left = ddong_x_pos
+    enemy_rect.top = ddong_y_pos
+
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect): # 충돌 했는지를 확인
+        running = False
+
+    
+    # 4. 충돌 처리
+
+    # 5. 화면에 그리기
+
+    screen.blit(background, (0, 0)) # 배경 그리기
+    screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기 
+    screen.blit(ddong, (ddong_x_pos, ddong_y_pos))  # 적 그리기
+
+
+    pygame.display.update() 
+
+# 잠시 대기
+pygame.time.delay(2000) # 2초 정도 대기 (ms)
+pygame.quit()
+
+
+###########################################################################
+# QUIZ) 하늘에서 떨어지는 똥 피하기 게임을 만드시오
+
+# [게임 조건]
+# 1. 캐릭터는 화면 가장 아래에 위치, 좌우로만 이동 가능
+# 2. 똥은 화면 가장 위에서 떨어짐, x 좌표는 매번 랜덤으로 설정
+# 3. 캐릭터가 똥을 피하면 다음 똥이 다시 떨어짐
+# 4. 캐릭터가 똥과 충돌하면 게임 종료
+# 5. FPS는 30으로 고정
+
+# [게임 이미지]
+# 1. 배경 : 640 * 480 (세로 가로) - background.png 
+# 2. 캐릭터 : 70 * 70 - character.png 
+# 3. 똥 : 70 * 70 - enemy.png 
